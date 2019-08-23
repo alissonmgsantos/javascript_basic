dce805e778b14f3aad7e63b986c8d586c1117544
Alisson Matos <alissonmgsantos@gmail.com>
2019-08-23 08:17:10 -0300

    Calculando imc do primeiro paciente

---
+++ js/principal.js
@@ -0,0 +1,14 @@
+let paciente = document.querySelector("#primeiro-paciente");
+
+let tdPeso = paciente.querySelector(".info-peso");
+let peso = tdPeso.textContent;
+
+let tdAltura = paciente.querySelector(".info-altura");
+let altura = tdAltura.textContent;
+
+
+let tdImc = paciente.querySelector(".info-imc");
+
+let imc = peso / (altura * altura);
+
+tdImc.textContent = imc;
