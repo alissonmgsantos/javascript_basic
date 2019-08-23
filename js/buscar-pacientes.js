10b896c60e8953fbd2f655ee5b6f727349896ba8
Alisson Matos <alissonmgsantos@gmail.com>
2019-08-23 15:29:27 -0300

    Ajuste da url

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -5,7 +5,7 @@ document.querySelector("#buscar-pacientes").addEventListener("click", function()
     console.log("Buscando pacientes...");
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "https://api-pacientes.herokuapp.com/paci1entes");
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
     
     xhr.addEventListener("load", function() {
         var erroAjax = document.querySelector("#erro-ajax");
